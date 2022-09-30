7e45236b3b0c1e62e549f5a8512d5145be6102bf
Nayana Ramesh <107012@tfcorp.com>
2022-09-30 18:25:49 +0530

    vue 3 and vue 2 version concepts

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <my-modal />
+</template>
+
+<script>
+import MyModal from './components/MyModal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyModal
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
